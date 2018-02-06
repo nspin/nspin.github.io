93eab5d733bc57fa3f49ae427cd5194b51bbfb57
Nick Spinale <nick@nickspinale.com>
2018-02-06 01:22:56 -0600

    Simplify compilation logic

--- Main.hs
+++ Main.hs
@@ -29,23 +29,30 @@ main = hakyllWith config $ do
 
     match "articles/*" $ do
         route $ setExtension ".html"
-        compile $ pandoc >>= stack defaultContext ["templates/article.html", "templates/content.html"]
+        compile $ pandoc
+            >>= loadAndApplyTemplate "templates/article.html" defaultContext
+            >>= loadAndApplyTemplate "templates/content.html" defaultContext
 
     create ["articles.html"] $ do
         route idRoute
         let ctx =  constField "title" "Articles"
                 <> listField "articles" defaultContext (loadAll "articles/*")
                 <> defaultContext
-        compile $ makeItem "" >>= stack ctx ["templates/articles.html", "templates/content.html"]
+        compile $ makeItem ""
+            >>= loadAndApplyTemplate "templates/article.html" defaultContext
+            >>= loadAndApplyTemplate "templates/content.html" defaultContext
 
   where
-    stack ctx tmplts = foldl (>=>) return $ map (flip loadAndApplyTemplate ctx) tmplts
+
     copy pattern = match pattern $ do
         route idRoute
         compile copyFileCompiler
+
     content pattern = match pattern $ do
         route idRoute
-        compile $ getResourceBody >>= loadAndApplyTemplate "templates/content.html" defaultContext
+        compile $ getResourceBody
+            >>= loadAndApplyTemplate "templates/content.html" defaultContext
+
     pandoc = pandocCompilerWith defaultHakyllReaderOptions $
         defaultHakyllWriterOptions
             { writerHTMLMathMethod = MathJax ""
