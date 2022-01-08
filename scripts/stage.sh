9093d6e20f7a915353253123309aeed727d405af
Nick Spinale <nick@nickspinale.com>
2022-01-08 22:17:26 +0000

    x

--- scripts/stage.sh
+++ scripts/stage.sh
@@ -4,7 +4,7 @@ set -e
 prefix=_site
 remote=staging
 branch=site
-url="https://github.com/nspin/www-staging.git"
+url="git@github.com:nspin/www-staging.git"
 
 git remote get-url $remote > /dev/null || git remote add $remote $url
 git subtree push --prefix $prefix $remote $branch
